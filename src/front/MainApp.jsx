94ad5b2e4f3d74bd62e5da49e18e92ba4a3db097
plazuu <nikolasenriquez123@gmail.com>
2025-04-09 14:00:48 -0400

    testing things out added a button that does nothing and some css

--- src/front/MainApp.jsx
+++ src/front/MainApp.jsx
@@ -1,13 +1,22 @@
+import './style.css';
+
+function Button() {
+    return <button className="fraudButton">fraud button</button>;
+}
+
 function App() {
-    return <>
-        <h1>Test</h1>
-        <p>
-            This webpage is for a fake stock trading bot.
-            The bot may eventually be usable, but for now, 
-            so that we don't get accused of fraud, I'm going
-            to be transparent and say the truth: The bot does not exist.
-        </p>
-    </>;
+    return (
+        <>
+            <h1>Test</h1>
+            <p>
+                This webpage is for a fake stock trading bot.
+                The bot may eventually be usable, but for now, 
+                so that we don't get accused of fraud, I'm going
+                to be transparent and say the truth: The bot does not exist.
+            </p>
+            <Button/>
+        </>
+    );
 }
 
 export default App;
