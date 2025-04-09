8caffe6d233ca7eb7f5ebf7c7511ea6bb9aaf77d
Ill-Come-Up-With-The-Name-Later <magidonovan@gmail.com>
2025-04-09 12:54:14 -0400

    React now works fully.

---
+++ src/front/MainApp.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return <>
+        <h1>Test</h1>
+        <p>
+            This webpage is for a fake stock trading bot.
+            The bot may eventually be usable, but for now, 
+            so that we don't get accused of fraud, I'm going
+            to be transparent and say the truth: The bot does not exist.
+        </p>
+    </>;
+}
+
+export default App;
